411419ef74036ce807417252d68921df5dcef0d2
yuresilva <nogueiranetoy@gmail.com>
2022-12-20 11:33:37 -0300

    att2

--- src/components/Question/Index.jsx
+++ src/components/Question/Index.jsx
@@ -27,76 +27,69 @@ export function Questions({ questions, onClick, value }) {
     // aqui você pode chamar a função que re-renderiza os dados da questão na tela
   };
 
-  return (
-    <>
-      {questions.list.forEach((element) => {
-        var filtro = element.exerciceo == valorResult ? element.id : "";
-        const filteredList = questions.list.filter(
-          (item) => item.id === filtro
+  function componentList() {
+    questions.list.forEach((element) => {
+      var filtro = element.exerciceo == valorResult ? element.id : "";
+      const filteredList = questions.list.filter((item) => item.id === filtro);
+      if (filteredList != 0) {
+        return (
+          <>
+            <div className={styles.QuestionsContent}>
+              <div className={styles.TitleContainer}>
+                <NavLink to="/">
+                  <House size={32} onClick={onClick} />
+                </NavLink>
+                <h2>Super Ensino</h2> <span>{currentQuestion + 1}</span> /
+                {questions.list.length}
+                <div>
+                  <p>ID: {filteredList.id}</p>
+
+                  <p>Dificuldade: {filteredList.difficulty}</p>
+                  <p>Pergunta: {filteredList.question}</p>
+                  <p>Resposta correta: {filteredList.correct}</p>
+                </div>
+              </div>
+              <span>Analise as afirmativas a seguir</span>
+              <p> {filteredList.question}</p>
+              <p>II -- Queria saber porque você faltou aula.</p>
+              <p>III -- Acordo cedo porque gosto de estudar.</p>
+              <p>Com releção ao uso dos porquês, estão corretas?</p>
+
+              <div className={styles.GrupResponse}>
+                <button className={styles.ResponseQuestion}>a</button>
+                <p>Apenas a III Apenas a III</p>
+              </div>
+              <div className={styles.GrupResponse}>
+                <button className={styles.ResponseQuestion}>b</button>
+                <p>Apenas a III Apenas a III</p>
+              </div>
+              <div className={styles.GrupResponse}>
+                <button className={styles.ResponseQuestion}>c</button>
+                <p>Apenas a III Apenas a III</p>
+              </div>
+              <div className={styles.GrupResponse}>
+                <button className={styles.ResponseQuestion}>d</button>
+                <p>Apenas a III</p>
+              </div>
+            </div>
+            <div className={styles.GrupButtons}>
+              <NavLink className={styles.GrupNexTBack} to="/">
+                <ArrowLeft size={32} />
+                Voltar
+              </NavLink>
+              <NavLink className={styles.GrupNexTBack} to="/">
+                Próximo
+                <ArrowRight size={32} />
+              </NavLink>
+            </div>
+                         
+          </>
         );
-        {
-          {
-            console.log(filteredList);
-          }
-          if (filteredList.length != 0) {
-            return (
-              <>
-                <div className={styles.QuestionsContent}>
-                  <div className={styles.TitleContainer}>
-                    <NavLink to="/">
-                      <House size={32} onClick={onClick} />
-                    </NavLink>
-                    <h2>Super Ensino</h2> <span>{currentQuestion + 1}</span> /
-                    {questions.list.length}
-                    <div>
-                      <p>ID: {filteredList.id}</p>
+      }
+    });
+  }
 
-                      <p>
-                        Dificuldade: {filteredList.difficulty}
-                        {console.log(filteredList.difficulty)}
-                      </p>
-                      <p>Pergunta: {filteredList.question}</p>
-                      <p>Resposta correta: {filteredList.correct}</p>
-                    </div>
-                  </div>
-                  <span>Analise as afirmativas a seguir</span>
-                  <p> {filteredList.question}</p>
-                  <p>II -- Queria saber porque você faltou aula.</p>
-                  <p>III -- Acordo cedo porque gosto de estudar.</p>
-                  <p>Com releção ao uso dos porquês, estão corretas?</p>
-
-                  <div className={styles.GrupResponse}>
-                    <button className={styles.ResponseQuestion}>a</button>
-                    <p>Apenas a III Apenas a III</p>
-                  </div>
-                  <div className={styles.GrupResponse}>
-                    <button className={styles.ResponseQuestion}>b</button>
-                    <p>Apenas a III Apenas a III</p>
-                  </div>
-                  <div className={styles.GrupResponse}>
-                    <button className={styles.ResponseQuestion}>c</button>
-                    <p>Apenas a III Apenas a III</p>
-                  </div>
-                  <div className={styles.GrupResponse}>
-                    <button className={styles.ResponseQuestion}>d</button>
-                    <p>Apenas a III</p>
-                  </div>
-                </div>
-                <div className={styles.GrupButtons}>
-                  <NavLink className={styles.GrupNexTBack} to="/">
-                    <ArrowLeft size={32} />
-                    Voltar
-                  </NavLink>
-                  <NavLink className={styles.GrupNexTBack} to="/">
-                    Próximo
-                    <ArrowRight size={32} />
-                  </NavLink>
-                </div>
-              </>
-            );
-          }
-        }
-      })}
-    </>
-  );
+  useEffect(() => {
+    componentList();
+  }, []);
 }
